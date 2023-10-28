15923d18f6cddef97109b4d1d030e095613074aa
Funshyaj <Funshoajayi29@gmail.com>
2023-10-28 10:34:14 +0100

    restructure and finishes.. ready for test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const startButton = document.getElementById("listen-btn");
 const sendMailButton = document.getElementById("send-mail-btn");
+const modal = document.getElementById("modal");
 const form = document.forms[0];
 const indicatorText = document.getElementById("text");
 const output = document.getElementById("output");
@@ -16,10 +17,10 @@ recognition.onstart = () => {
 // Event handler when speech is recognized
 recognition.onresult = (event) => {
   const result = event.results[event.results.length - 1][0].transcript;
-  if(output.innerText == ''){
-    output.textContent = result[0].toUpperCase() + result.slice(1);
+  if (output.innerText == "") {
+    output.innerHTML = result[0].toUpperCase() + result.slice(1) + ",";
   } else {
-    output.textContent += "\n " + result;
+    output.innerHTML += " <br> " + result;
     // output.textContent += ", " + result;
     // features format text base on words used
     // words like 'new line' or 'fullstop' and 'comma'.
@@ -42,25 +43,32 @@ let started = false;
 startButton.addEventListener("click", () => {
   if (started) {
     recognition.stop();
-    started = false
-    startButton.classList.remove('recording')
-    startButton.ariaLabel = 'Click to start speaking'
-  } else {
+    started = false;
+    startButton.classList.remove("recording");
+    startButton.ariaLabel = "Click to start speaking";
+  } else if (navigator.onLine) {
     recognition.start();
-    started = true
-    startButton.ariaLabel = 'Listening to your message'
-    startButton.classList.add('recording')
+    started = true;
+    startButton.ariaLabel = "Listening to your message";
+    startButton.classList.add("recording");
+  } else {
+    startButton.ariaLabel = "No network, please connect to the Internet";
+    indicatorText.textContent = "No network, please connect to the Internet";
   }
 });
 
 // mail sending button
 form.addEventListener("submit", (e) => {
-e.preventDefault()
-})
+  e.preventDefault();
+  modal.style.display = "none";
+});
 
-sendMailButton.addEventListener('click', () => {
-console.log(form.elements['mail receiver'].value)
-  // location.href=`mailto:${form.elements['mail receiver'].value}`
-  location.href='mailto:funshoajayi29@gmail.com'
-})
+sendMailButton.addEventListener("click", () => {
+  console.log(
+    form.elements["mail receiver"].value,
+    form.elements["mail subject"].value,
+    output.innerText
+  );
 
+  location.href = `mailto:${form.elements["mail receiver"].value}?subject=${form.elements["mail subject"].value}&body=${output.innerText}`;
+});
